1d623aac76f2f92c1a69dd63a8fbf4efa93cee9c
cabien-dev <cabien13072000@gmail.com>
2024-03-31 21:51:05 +0700

    add: config es-lint, prettier

--- react-js-app/.eslintrc.cjs
+++ react-js-app/.eslintrc.cjs
@@ -1,18 +1,42 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2021: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'prettier',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', '.prettierrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'import', 'prettier'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    indent: [
+      'error',
+      2,
+      { SwitchCase: 1, ignoredNodes: ['ConditionalExpression'] },
+    ], // 2 spaces for indentation
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc', caseInsensitive: true },
+      },
+    ], // Config for alphabetizing modules
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-explicit-any': ['off'],
   },
-}
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
